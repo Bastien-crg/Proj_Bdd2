3484d26cf8cc91ad8d1b9bf984e9622085706ca0
Rwouaf <76561303+Rwouaf@users.noreply.github.com>
2021-04-26 15:14:43 +0200

    les 2 formulaire sur une page

--- projetPHP/formulaire.php
+++ projetPHP/formulaire.php
@@ -4,6 +4,7 @@
     <title>formulaire</title>
 </head>
 <body>
+    <!-- connexion -->
     <form class="formulaire" method="POST" action="connexion.php">
     <div class="email">
         <span class="input_email">E-mail</span>
@@ -19,6 +20,38 @@
             Login
         </button>
     </div>
+    </form>
+
+    <!-- inscription -->
+    <form class="formulaire" method="POST" action="#">
+
+
+        <div class="form_group">
+                <label for="speudo">Speudo</label>
+                <input type="text" id="speudo" name="speudo">                    
+        </div>
+
+        <div class="form_group">
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email">                    
+        </div>
+
+        <div class="form_group">
+                <label for="tel">Tel</label>
+                <input type="text" id="tel" name="tel">                    
+        </div>
+
+        <div class="form_group">
+                <label for="adresse">Adresse</label>
+                <input type="text" id="adresse" name="adresse">                    
+        </div>
+
+        <div class="form_group">
+            <label for="mdp">MDP</label>
+            <input name="message" id="message" cols="30" rows="10"></input>                 
+        </div>
+
+        <button type="submit">inscription</button>
     </form>   
 </body>
 </html>
