e6a39a0ec141af7b57ebb713c3951f9d9d928488
Bastien <bastien.corgnac@gmail.com>
2021-05-04 23:00:16 +0200

    test

--- projetPHP/updateMachine.php
+++ projetPHP/updateMachine.php
@@ -10,21 +10,24 @@
 		echo $_GET["fonctionnement"];
 
 		if (isset($_GET["nummachine"]) && isset($_GET["fonctionnement"])) {
-			include("connexion.inc.php");
 
-			$requete="\c rdirezdu_db";
-			$result=$cnx->query($requete);
-			$requete="set search_path to projet ;";	
-			$result=$cnx->query($requete);
-			if ($_GET["fonctionnement"] == "En service") {
-				$requete="update machines set etats_machine = 'E.S' where nummachine = ".$_GET["nummachine"].";";
-			} else {
-				$requete="update machines set etats_machine = 'H.S' where nummachine = ".$_GET["nummachine"].";";
+			function maj($arg1, $arg2){
+				include("connexion.inc.php");
+				$requete="\c rdirezdu_db";
+				$result=$cnx->query($requete);
+				$requete="set search_path to projet ;";	
+				$result=$cnx->query($requete);
+				if ($_GET["fonctionnement"] == "En service") {
+					$requete="update machines set etats_machine = 'E.S' where nummachine = ".$arg1.";";
+				} else {
+					$requete="update machines set etats_machine = 'H.S' where nummachine = ".$arg2.";";
+				}
+				$result=$cnx->query($requete);
 			}
-			$result=$cnx->query($requete);
 
-			
-			
+
+			maj($_GET["nummachine"],$_GET["fonctionnement"]);
+
 		}
 		header('location: https://etudiant.u-pem.fr/~bcorgnac/projetPHP/controleAdmin.php');
 
